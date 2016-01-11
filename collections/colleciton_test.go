7a2bea9328ae2a261f495ba3b00877fb1f9f74d9
jolestar <jolestar@gmail.com>
2016-01-11 15:46:13 +0800

    travis cfg

--- collections/colleciton_test.go
+++ collections/colleciton_test.go
@@ -1,1 +1,19 @@
 package collections
+import (
+	"testing"
+	"fmt"
+)
+
+func TestMapLength(t *testing.T){
+	m := make(map[int]int, 1)
+	fmt.Println(len(m))
+	m[0] = 0
+	m[1] = 1
+	fmt.Println(len(m))
+}
+
+func TestSliceLength(t *testing.T){
+	m := make([]int, 10)
+	fmt.Println(len(m))
+
+}
